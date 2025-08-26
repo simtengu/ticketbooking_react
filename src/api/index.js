ac047bc58ab6fe03bdde2edeeeeb941fe61e0806
albert oscar simtengu <albertsimtengu@gmail.com>
2025-08-27 00:35:25 +0300

    Update index.js updated base urls

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,14 @@ import axios from "axios"
 
 let publicApi = axios.create({
     // baseURL: "http://localhost:5000/api",
-    baseURL: "https://simtenguticketbooking.cyclic.app/api",
+    baseURL: "https://ticketbooking-backend-oza9.onrender.com/api",
     withCredentials: true,
 
 })
 
 let secureApi = axios.create({
     // baseURL: "http://localhost:5000/api",
-    baseURL: "https://simtenguticketbooking.cyclic.app/api",
+    baseURL: "https://ticketbooking-backend-oza9.onrender.com/api",
     withCredentials: true,
   
 })
